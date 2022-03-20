1beecc6a5512cdd4d1ae975c07ed404629031720
karim <karim-megri@hotmail.fr>
2022-03-20 13:45:47 +0100

    laboutique: ajout easyadmin et ajout des produits

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -29,5 +31,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
          yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
+         yield MenuItem::linkToCrud('Categories', 'fas fa-list', Category::class);
+         yield MenuItem::linkToCrud('Products', 'fas fa-tag', Product::class);
     }
 }
